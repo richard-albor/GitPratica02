a7083d6d95bc353a650feab9dc897656701badc6
NoteRichard <NoteRichard@noteRich>
2020-10-21 17:59:01 -0300

    Salvando alterações iniciais do passo 2 da atividade

---
+++ Git Pratica 02/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Primeira Classe do projeto Git Básico 02");
+		System.out.println("Modificação 1");
+		System.out.println("Modificação 2");
+		System.out.println("Modificação 3");
+
+	}
+
+}

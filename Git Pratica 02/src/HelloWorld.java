d11813257f0445f9430ee2d18ec61acb170c26cb
NoteRichard <NoteRichard@noteRich>
2020-10-21 18:09:54 -0300

    Finalizando etapa 4 do passo 3 para ganhar a nota :)

--- Git Pratica 02/src/HelloWorld.java
+++ Git Pratica 02/src/HelloWorld.java
@@ -2,10 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Primeira Classe do projeto Git Básico 02");
-		System.out.println("Modificação 1");
-		System.out.println("Modificação 2");
-		System.out.println("Modificação 3");
+		System.out.println("Passo 3 - finalizando etapa 4 para ganhar notas :) ");
 
 	}
 

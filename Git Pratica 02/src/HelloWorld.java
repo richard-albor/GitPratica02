b3f1307b10a08de9e6da4b42c656ce9a15f33d72
NoteRichard <NoteRichard@noteRich>
2020-10-28 14:58:20 -0300

    Prática 03 - Aluno Richar F Albor

--- Git Pratica 02/src/HelloWorld.java
+++ Git Pratica 02/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Passo 3 - finalizando etapa 4 para ganhar notas :) ");
+		
+		System.out.println("Passo 2 da parte  1. Prática 03 - Aluno Richar F Albor");
 
 	}
 
